b15d6af8139dfc87862fdb33f8c844a2353c5917
bluebead421@gmail.com <dikshab463@gmail.com>
2026-02-07 15:28:18 +0530

    msg

--- class2.java
+++ class2.java
@@ -1,6 +1,8 @@
 import java.util.*;
+
 public class class2 {
-     Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Array ka size: ");
         int n = sc.nextInt();
@@ -8,36 +10,40 @@ public class class2 {
         int[] arr = new int[n];
 
         System.out.println("Array elements:");
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-        };
-        
-         for(int i = 0; i < n; i++) {
-            if(arr[i] % 2 == 0){
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
                 System.out.println("I am even " + arr[i]);
-            }else{
-                 System.out.println("I am odd " + arr[i]);
-            };
-        };
-        
-        //searching
+            } else {
+                System.out.println("I am odd " + arr[i]);
+            }
+        }
+
+        // searching
         boolean found = false;
-        System.out.println("Enter element you want to find");
+        System.out.println("Enter element you want to find:");
         int num = sc.nextInt();
-        for(int i=0; i<n; i++){
-            if(arr[i] == num){
-                System.out.println("Element found at index "+i);
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == num) {
+                System.out.println("Element found at index " + i);
                 found = true;
                 break;
-            };
-            
-          
-        };
+            }
+        }
+        
+        if (!found) {
+            System.out.println("Element not found.");
+        }
+
         // print array
         System.out.println("Array hai:");
-        for(int i = 0; i < n; i++) {
-           System.out.print(arr[i] + " ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 }
     
---
+++ class3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class class3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int left = 0;
+        int right = n - 1;
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+        System.out.println("Reversed array is");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
